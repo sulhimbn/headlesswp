0b52456c47ad6aaf8fb44c320398891199557013
sulhimbn <237669368+sulhimbn@users.noreply.github.com>
2026-01-07 17:12:12 +0000

    FIX: Add environment variables to jest setup for tests

--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,10 @@
 import '@testing-library/jest-dom'
 
+process.env.WORDPRESS_URL = 'http://localhost:8080'
+process.env.WORDPRESS_API_URL = 'http://localhost:8080/wp-json'
+process.env.NEXT_PUBLIC_WORDPRESS_URL = 'http://localhost:8080'
+process.env.NEXT_PUBLIC_WORDPRESS_API_URL = 'http://localhost:8080/wp-json'
+
 // Mock Next.js server components
 jest.mock('next/server', () => ({
   NextRequest: class MockRequest {
